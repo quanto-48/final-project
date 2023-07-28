66d21942d00ecec4d211bd4385ac7eaf74e4c711
quanto-48 <133702421+quanto-48@users.noreply.github.com>
2023-07-28 15:21:14 +0200

    first

---
+++ data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Facial portrait",
+      price: 45,
+      desc: "So when you see your portrait you realise who you are truly.",
+      img: "images/img-1.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Nature portrait",
+      price: 100,
+      desc: "Beautiful photos in the nature.",
+      img: "images/img-2.jpg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "Wedding portrait",
+      price: 25,
+      desc: "To remember the good times in weddings.",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Fashion portrait",
+      price: 300,
+      desc: "To extend your knoledge about fashion.",
+      img: "images/img-4.jpg",
+    },
+  ];
